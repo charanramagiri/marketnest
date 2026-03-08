5fca0637ceab60af4a05b6b52bfe7d9ec362a476
charan <charanramagiri2004@gmail.com>
2026-03-08 12:20:58 +0530

    solved imports

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import API from "../api/axios";
+import API from "../api/api";
 import { clearAuth } from "../utils/auth";
 
 export default function Signup() {
