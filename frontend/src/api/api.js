7df88517b3fd5436499cec0a84bfc9e6c64ea9e9
charan <charanramagiri2004@gmail.com>
2026-06-13 14:19:33 +0530

    testing for local and deployed running

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,3 +1,17 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5000/api"
+      : "https://marketnest-backend-htxq.onrender.com/api",
+
+  withCredentials: true
+});
+
+export default API;
+
+
 // import axios from "axios";
 
 // const API = axios.create({
@@ -6,11 +20,13 @@
 // });
 
 // export default API;
-import axios from "axios";
 
-const API = axios.create({
-  baseURL: "http://localhost:5000/api",
-  withCredentials: true
-});
 
-export default API;
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://localhost:5000/api",
+//   withCredentials: true
+// });
+
+// export default API;
